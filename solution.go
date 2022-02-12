a9fc47a37f09932839188c07827ce8f97d6a6bc9
Astapova M <astap.m92@gmail.com>
2022-02-12 20:53:10 +0600

    edit solution.go

--- solution.go
+++ solution.go
@@ -1,9 +1,9 @@
 package solution
 
 import (
-	"github.com/kyokomi/emoji/v2"
+	emoji "github.com/kyokomi/emoji"
 )
 
 func GetMessage() string {
-	return emoji.Sprint("Hello, :world_map:!")
+	return emoji.Sprint("Hello :world_map:!")
 }
